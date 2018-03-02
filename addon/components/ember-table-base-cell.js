f0b1e2b51f27d15a73e82a00378d70da5e057e61
Billy Pham <billy.pham@addepar.com>
2018-03-02 10:44:37 -0800

    Remove row height observer for fixed cell

--- addon/components/ember-table-base-cell.js
+++ addon/components/ember-table-base-cell.js
@@ -2,7 +2,6 @@ import Component from '@ember/component';
 import { addObserver, removeObserver } from '@ember/object/observers';
 import { htmlSafe } from '@ember/string';
 import { get } from '@ember/object';
-import { scheduler, Token } from 'ember-raf-scheduler';
 
 import { action, computed } from '@ember-decorators/object';
 import { tagName, attribute } from '@ember-decorators/component';
@@ -34,41 +33,6 @@ export default class EmberTableCell extends Component {
   @argument
   @type(optional('object'))
   cell;
-
-  constructor() {
-    super(...arguments);
-
-    this.token = new Token();
-  }
-
-  willDestroy() {
-    this.token.cancel();
-  }
-
-  didInsertElement() {
-    if (this.get('isFixed')) {
-      this.scheduleSync();
-
-      // TODO: For now we are just watching rows for changes in height, but
-      // theoretically anything can change the height of a row. We need
-      // to come up with a better solution.
-      addObserver(this, 'rowValue', this, this.scheduleSync);
-    }
-  }
-
-  willDestroyElement() {
-    if (this.get('isFixed')) {
-      removeObserver(this, 'rowValue', this, this.scheduleSync);
-    }
-  }
-
-  scheduleSync() {
-    scheduler.schedule('sync', () => {
-      let { height } = this.element.getBoundingClientRect();
-
-      this.set('cellHeight', height || 0);
-    }, this.token);
-  }
 
   @computed('columnIndex', 'numFixedColumns')
   get isFixed() {
